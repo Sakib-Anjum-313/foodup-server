afa9397edbc13ca4c54b609d8ea942d01c64c089
Sakib-Anjum-313 <sakibanjum.313@gmail.com>
2023-02-17 23:03:56 +0600

    vercel env fix

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
 // Midleware
 app.use(cors());
