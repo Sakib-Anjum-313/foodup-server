0fcda09bc79c700c70b8daecb44613f79ea0cfcb
Sakib-Anjum-313 <sakibanjum.313@gmail.com>
2023-02-18 14:47:18 +0600

    vercel pss

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.use(express.json());
 // db id: foodup
 // pass: NPqRWveqj0FgUsI2
 
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dmfze9b.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
